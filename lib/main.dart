e1b855ccd473bfc04bde23e27923b3181b081139
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-06-25 12:33:34 +0530

    Added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:chat_app/styles/theme_style.dart';
 import 'package:chat_app/views/auth_view.dart';
 import 'package:chat_app/views/home_view.dart';
 import 'package:chat_app/views/unknown_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
